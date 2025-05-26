4651cff8df27fc4dbf6994b84c4b4bdf305c45d6
VICODER <20243ds066@utez.edu.mx>
2025-05-26 09:41:53 -0600

    Se agregó la clase computadora

--- U1_TI_04_Computadora/src/main/java/org/example/Main.java
+++ U1_TI_04_Computadora/src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import Model.Computadora;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Computadora pc = new Computadora();
+        pc.setModel("Victus");
+        pc.turnOn();
+        pc.turnOn();
+        pc.turnOff();
+        System.out.println("Marca: "+pc.getBrand());
+        System.out.println("Modelo: "+pc.getModel());
+        System.out.println("Encendida: "+(pc.getStatus()?"Si": "No"));
     }
 }
