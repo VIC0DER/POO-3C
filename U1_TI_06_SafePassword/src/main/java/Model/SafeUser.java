4cdbdf20463c28c05f124759c8346e1d93c38394
VIC0DER <20243ds066@utez.edu.mx>
2025-05-29 09:50:30 -0600

    Proyecto U1_TI_06_SafePassword terminado.

---
+++ U1_TI_06_SafePassword/src/main/java/Model/SafeUser.java
@@ -0,0 +1,49 @@
+package Model;
+
+public class SafeUser {
+    private String username = "";
+    private String password = "";
+    public void setUsername(String username) {
+        if (username != null || !username.isEmpty()) {
+            this.username = username;
+        }else{
+            System.out.println("El usuario no puede estar vacio");
+        }
+    }
+
+    public void setPassword(String password) {
+        if(password.length()>=8 && checkPassword(password)){
+            this.password = password;
+        }else{
+            System.out.println("La contrasena no cumple con las condiciones");
+        }
+    }
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+
+    public boolean checkPassword(String password){
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean number = false;
+        int codeAscii;
+        for (int i = 0; i < password.length(); i++) {
+            codeAscii = password.charAt(i);
+            if(codeAscii >= 65 && codeAscii <= 90){
+                upperCase = true;
+            }else if(codeAscii >= 97 && codeAscii <= 122){
+                lowerCase = true;
+            }else if(codeAscii >= 48 && codeAscii <= 57){
+                number = true;
+            }
+        }
+        return upperCase && lowerCase && number;
+    }
+
+    public boolean authentication(String username, String password){
+        return username.equals(this.username) && password.equals(this.password);
+    }
+}
